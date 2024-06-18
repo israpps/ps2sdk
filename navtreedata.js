0a9fad1df0491812145df7ef9d890d249130077f
israpps <israpps@users.noreply.github.com>
2024-06-18 12:17:17 +0000

    deploy: d367178b6a6e904083a0e33c9e257b268b227a50

---
+++ navtreedata.js
@@ -0,0 +1,87 @@
+/*
+ @licstart  The following is the entire license notice for the JavaScript code in this file.
+
+ The MIT License (MIT)
+
+ Copyright (C) 1997-2020 by Dimitri van Heesch
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ @licend  The above is the entire license notice for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "PS2SDK", "index.html", [
+    [ "PS2SDK - PS2DEV Open Source Project", "index.html", null ],
+    [ "PS2SDK - KERNEL LIBRARY", "md_ee_2kernel_2_r_e_a_d_m_e.html", null ],
+    [ "remote", "md_ee_2rpc_2remote_2remote.html", null ],
+    [ "Topics", "topics.html", "topics" ],
+    [ "Data Structures", "annotated.html", [
+      [ "Data Structures", "annotated.html", "annotated_dup" ],
+      [ "Data Structure Index", "classes.html", null ],
+      [ "Data Fields", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", null ],
+        [ "Variables", "functions_vars.html", "functions_vars" ]
+      ] ]
+    ] ],
+    [ "Files", "files.html", [
+      [ "File List", "files.html", "files_dup" ],
+      [ "Globals", "globals.html", [
+        [ "All", "globals.html", "globals_dup" ],
+        [ "Functions", "globals_func.html", "globals_func" ],
+        [ "Variables", "globals_vars.html", null ],
+        [ "Typedefs", "globals_type.html", null ],
+        [ "Enumerations", "globals_enum.html", null ],
+        [ "Enumerator", "globals_eval.html", null ],
+        [ "Macros", "globals_defs.html", "globals_defs" ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"__start_8c_source.html",
+"atahw_8h.html#a414747b879d890c1df55ccbd4208cf1ca8e87784e330f5247e0212f82785a9493",
+"audsrv_8c.html#ae538f96c6438e7b0ed26269a283afc09",
+"dir_031757951e60c16ad930ba76071ecfef.html",
+"dir_83706370b12deb47497ed91304cced4b.html",
+"dma_8h.html#aa0f72b3bcb8c0471e15d385834dedbdc",
+"ee_2network_2tcpip_2src_2include_2arch_2sys__arch_8h_source.html",
+"ee__regs_8h.html#adb95789480c1736bc2d2a18b5cae1693",
+"file_xio_8h.html#structfxio__readlink__packet",
+"globals_func_l.html",
+"group__packet2__types.html#ac17432d7df9536ba931637e81c798bb4",
+"gs__psm_8h.html#acffebed01fb058b25431bd08d44443c1",
+"ioman_8h.html",
+"iop_2system_2excepman_2src_2irx__imports_8h_source.html",
+"kernel_8h_source.html",
+"libgs_8h.html#a03cdf59055bc8cf79f9e70143ffbf72c",
+"libgs_8h.html#acf5840e196f070298e510850842e5eb5",
+"libkbd_8h.html#a8300e61f29bf0ef39c5e455b5285c053",
+"libpad_8c.html#structpad__data",
+"modload_8h.html#structloadfile__elf32__phdr__",
+"ps2__debug_8h_source.html",
+"ps2ipee_8h_source.html",
+"s__svv_8c_source.html",
+"smapregs_8h.html#aa4833a8606567721cff029346a9ee49c",
+"tcpip_8h.html#a0bb00f48d6ba1e8c55b7d85c8e3a19a7",
+"usbd_8h.html#struct_usb_hub_descriptor"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
